d3b06291245796a18ad99da202081fa2eac20e6a
satoru gojo <eternalblessed99@gmail.com>
2024-02-08 20:09:28 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -3,110 +3,62 @@
 
 using namespace std;
 
-class Animal
+template<typename T, typename F>
+vector<T> myMap(vector<T> &inputVector, F callback)
 {
-protected:
-    string name;
-    int age;
-public :
-    virtual string makeSound() = 0;
-};
+    vector<T> answer;
+    for (T &element: inputVector) {
+        answer.push_back(callback(element));
+    }
+    return answer;
+}
 
-class LandAnimal: public virtual Animal
+template<typename T, typename F>
+vector<T> myFilter(vector<T> &inputVector, F callback)
 {
-public:
-    virtual string sunbathe() = 0;
-    string makeSound() override
-    {
-        return "Top-top";
+    vector<T> answer;
+    for (T element: inputVector) {
+        if (callback(element)) {
+            answer.push_back(element);
+        }
     }
-};
-class WaterAnimal: public virtual Animal
-{
-public:
-    virtual string breath() = 0;
-    string makeSound() override
-    {
-        return "Guuuurgle";
-    }
-};
-
-class Lion: public LandAnimal
-{
-public:
-    Lion(int age, string name)
-    {
-        this->age = age;
-        this->name = name;
-    }
-    string sunbathe() override
-    {
-        return "Aaaaaaaa-Hhhhhhhhhhhh-Rrrrrrrr";
-    }
-};
-class Dolphin: public WaterAnimal
-{
-public:
-    Dolphin(int age, string name)
-    {
-        this->age = age;
-        this->name = name;
-    }
-    string breath() override
-    {
-        return "Eeeee-Eeee-Eeee";
-    }
-
-};
-class Frog: public LandAnimal, public WaterAnimal
-{
-public:
-    Frog(int age, string name)
-    {
-        this->age = age;
-        this->name = name;
-    }
-    string makeSound() override
-    {
-        return "Croooooooaaaaaaaaak";
-    }
-    string breath() override
-    {
-        return "The frog is breathing";
-    }
-    string sunbathe() override
-    {
-        return "The frog is chilling during sunbathing";
-    }
-};
+    return answer;
+}
 
 int main()
 {
-    vector<Animal *> zoo;
-    Frog frog(12, "Clava");
-    Dolphin dolphin(33, "Bone");
-    Lion lion(24, "Gone");
-    zoo.push_back(&frog);
-    zoo.push_back(&dolphin);
-    zoo.push_back(&lion);
-    for ( auto &animal: zoo) {
-        animal->makeSound();
-        if (auto creature = dynamic_cast<Dolphin *>(animal)) {
-            cout << creature->makeSound() << endl;
-            cout << creature->breath() << endl;
-            cout << "\n";
-        }
-        if (auto creature = dynamic_cast<Frog *>(animal)) {
-            cout << creature->makeSound() << endl;
-            cout << creature->breath() << endl;
-            cout << creature->sunbathe() << endl;
-            cout << "\n";
-        }
-        if (auto creature = dynamic_cast<Lion *>(animal)) {
-            cout << creature->makeSound() << endl;
-            cout << creature->sunbathe() << endl;
-            cout << "\n";
-        }
+    vector<int> example = {2, 5, 6, 8, 13};
+    auto funcSample1 = [](int x) // Squared all integers in the vector
+    { return x * x; };
+    auto mapSample1 = myMap(example, funcSample1);
+    cout << "First map test:" << "\n";
+    for (int num: mapSample1) {
+        cout << "Current num is: " << num << "\n";
     }
+
+    auto funcSample2 = [](int x)// Multiply by 5 all integers in the vector
+    { return x * 5; };
+    auto mapSample2 = myMap(example, funcSample2);
+    cout << "\n" << "Second map test:" << "\n";
+    for (int num: mapSample2) {
+        cout << "Current num is: " << num << "\n";
+    }
+
+    auto funcSample3 = [](int x)// Delete all even integers from the vector
+    { return x % 2 != 0; };
+    auto filterSample1 = myFilter(example, funcSample3);
+    cout << "\n" << "First filter test:" << "\n";
+    for (int num: filterSample1) {
+        cout << "Current num is: " << num << "\n";
+    }
+
+    auto funcSample4 = [](int x)// Delete all integers less than 6 from the vector
+    { return x > 5; };
+    auto filterSample2 = myFilter(example, funcSample4);
+    cout << "\n" << "Second filter test:" << "\n";
+    for (int num: filterSample2) {
+        cout << "Current num is: " << num << "\n";
+    }
+
     return 0;
 }
